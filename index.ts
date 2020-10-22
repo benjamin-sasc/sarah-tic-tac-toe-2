9645c72e86e81c517e9242748beb0678b6f1adbf
benjamin-sasc <benjamin-sasc@dataindependence.net>
2020-10-21 19:32:29 -0600

    1 file modified

--- index.ts
+++ index.ts
@@ -5,11 +5,23 @@ for (var tdElement of tdElements) {
 }
 function handleClick(event) {
   var target = event.target;
-  if (isO) {
-    
-  }
 
+    
   target.innerHTML = "O"
+  var arrayOfAnything = [
+  1,
+  true,
+  "a",
+  "hello",
+  {
+    testProperty: false
+  },
+  function() {
+    alert("I'm a function");
+  }
+];
+
+// alert("arrayOfAnything.length: " + arrayOfAnything.length); 
 
 
 }
